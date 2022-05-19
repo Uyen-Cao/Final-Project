b6bf9c97de1b16857d906c8dd629532ac0ff73c9
N.Thang Ng <75669752+Thang1902@users.noreply.github.com>
2022-05-19 08:46:16 +0700

    Add files via upload

--- src/main/java/utils/presentArticleMethod.java
+++ src/main/java/utils/presentArticleMethod.java
@@ -1,5 +1,7 @@
 package utils;
 
+import com.rometools.rome.feed.rss.Content;
+import com.rometools.rome.feed.rss.Item;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
@@ -7,34 +9,60 @@ import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 import models.Article;
 
+import java.io.Closeable;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+
 public class presentArticleMethod {
-    public static List<Article> presentArticles() throws Exception {
+    public static List<Article> presentArticles(String url) throws Exception {
         List<Article> ls = new ArrayList<>();
+        int count = 0;
 
-        String url = "https://vnexpress.net/rss/tin-moi-nhat.rss";
-
+        //String url = "https://vnexpress.net/rss/tin-moi-nhat.rss";
         URL feedUrl = new URL(url);
+
+
         SyndFeedInput input = new SyndFeedInput();
         SyndFeed feed = input.build(new XmlReader(feedUrl));
-        for (SyndEntry entry : (List<SyndEntry>) feed.getEntries()) {
+        List<SyndEntry> entry = feed.getEntries();
+
+//        for (SyndEntry entry : (List<SyndEntry>) feed.getEntries()) {
+//            System.out.println("-----------------------------");
+//            System.out.println("Title : " + entry.getTitle());
+//            System.out.println("Author: "+entry.getDescription().getValue());
+//            String convertedURL = getURLInString(entry.getDescription().getValue());
+//            System.out.println("Link URL  : "+convertedURL);
+//            if(convertedURL == null){
+//                convertedURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
+//            }
+//            System.out.println("Link  : "+entry.getLink());
+//            Article newArticle = new Article("123", entry.getTitle(), entry.getUri(), convertedURL);
+//            ls.add(newArticle);
+//
+//        }
+
+        for (int i = 0; i < 10; i++) {
             System.out.println("-----------------------------");
-            System.out.println("Title : " + entry.getTitle());
-            System.out.println("Author: "+entry.getDescription().getValue());
-            String convertedURL = getURLInString(entry.getDescription().getValue());
+            System.out.println("Title : " + entry.get(i).getTitle());
+            System.out.println("Author: "+entry.get(i).getDescription().getValue());
+            String convertedURL = getURLInString(entry.get(i).getDescription().getValue());
             System.out.println("Link URL  : "+convertedURL);
             if(convertedURL == null){
                 convertedURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
             }
-            System.out.println("Link  : "+entry.getLink());
-            Article newArticle = new Article("123", entry.getTitle(), entry.getUri(), convertedURL);
+            System.out.println("Link  : "+entry.get(i).getLink());
+            //Article newArticle = new Article("123", entry.get(i).getTitle(), entry.get(i).getUri(), convertedURL);
+            Article newArticle = new Article("123", entry.get(i).getTitle(), entry.get(i).getUri(), "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png");
             ls.add(newArticle);
         }
+
         return ls;
     }
 
@@ -53,4 +81,6 @@ public class presentArticleMethod {
         }
         return null;
     }
+
+
 }
